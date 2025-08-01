2782e8ac8864e7f18b455d287e964b21847edc9e
poorni172 <cpoorni1306@gmail.com>
2025-08-01 14:43:47 +0530

    Add files via upload

---
+++ experiment 3.py
@@ -0,0 +1,6 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b")) 
+c=int(input("enter the value of c")) 
+if (a<b and a<c):print("a is the smallest") 
+if (b<a and b<c):print("b is the smallest")  
+if (c<a and c<b):print("c is the smallest")                   
